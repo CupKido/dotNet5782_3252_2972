f547d10bcd6df7e2d3e6acbc36fe1e9284273856
saar taler <taler@g.jct.ac.il>
2021-10-03 20:49:05 +0300

    initial code + welcome functions

---
+++ dotNet5782_3252_2972/Targil0/Program3252.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3252();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome2972();
+
+        private static void Welcome3252()
+        {
+            Console.WriteLine("Enter Your Name: ");
+            Console.WriteLine("{0}, welcome to my first console application", Console.ReadLine());
+        }
+    }
+}
