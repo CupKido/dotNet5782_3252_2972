fd94aad878047e954715938c0fae9dedb717e961
Josef Wolf <jwolf@g.jct.ac.il>
2021-12-28 22:40:43 +0200

    Base Station window complete

--- dotNet5782_3252_2972/PL/BaseStation/ShowBaseStationWindow.xaml.cs
+++ dotNet5782_3252_2972/PL/BaseStation/ShowBaseStationWindow.xaml.cs
@@ -52,15 +52,58 @@ namespace PL.BaseStation
         }
         private void AddBaseStation_Button_Click(object sender, RoutedEventArgs e)
         {
+            int BsId , numOfCharges;
+            double latitude, longitude;
+            BO.Location location = new BO.Location();
+            
+            if (!int.TryParse(BaseStationId_TextBox.Text, out BsId))
+            {
+                MessageBox.Show("ID must be a Number!", "Wrong ID type", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
+            if (BaseStationName_TextBox.Text == "")
+            {
+                MessageBox.Show("Please enter the Base Station's name", "Empty name value", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            if (!double.TryParse(BaseStationLongitude_TextBox.Text, out longitude))
+            {
+                MessageBox.Show("Longitude must be a Number!", "Wrong Longitude type", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (!double.TryParse(BaseStationLatitude_TextBox.Text, out latitude))
+            {
+                MessageBox.Show("LAtitude must be a Number!", "Wrong LAtitude type", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (!int.TryParse(BaseStationChargeSlots_TextBox.Text, out numOfCharges))
+            {
+                MessageBox.Show("Number of charges must be a Number!", "Wrong Number of charges type", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            location.Latitude = latitude;
+            location.Longitude = longitude;
+            try
+            {
+                myBL.AddBaseStations(BsId, BaseStationName_TextBlock.Text, location, numOfCharges);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Exception ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            CloseWindow();
         }
         public void CloseWindow()
         {
             disallowClosure = false;
             this.Close();
         }
-    
 
-
+       
     }
 }
